d46170bc4b581b7da39d7ec313c85e625eeb4b8e
DEVELoper <syabblogger@gmail.com>
2022-12-21 23:26:12 +0500

    small design applied

--- app/src/main/java/com/example/gpacalculatoruos/MainActivity.java
+++ app/src/main/java/com/example/gpacalculatoruos/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Feed Back will be added Shortly", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
